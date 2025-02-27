c3ab66654362f7793ec9d7f3b6f9832325ed62a0
febitriwahyu54 <febitriwhyu9a@students.unnes.ac.id>
2025-02-28 01:11:18 +0700

    fitur shoping search

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-// togle class active
+// togle class active hamburger menu
 const navbarnav = document.querySelector('.navbar-nav');
 // ketika hamburger menu diklik
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarnav.classList.toggle('active');
 };
 
-// klik dilur sidebar
+// togle class active untuk search form
+const searchform = document.querySelector('.search-form');
+const searchbox = document.querySelector('.#search-box');
+
+document.querySelector('#search-button').onclick = () => {
+    searchform.classList.toggle('active');
+    searchbox.focus();
+}
+
+// klik diluar sidebar
 const hamburgermenu =document.querySelector('#hamburger-menu');
 document.addEventListener('click', function(e) {
     if(!hamburgermenu.contains(e.target) && !navbarnav.contains(e.target)){
