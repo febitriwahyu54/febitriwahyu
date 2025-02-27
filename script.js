d8b48ad78c5e101a218be5f22e59e1ecd398184c
febitriwahyu54 <febitriwhyu9a@students.unnes.ac.id>
2025-02-28 01:14:04 +0700

    fitur search

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.querySelector('#hamburger-menu').onclick = () => {
 
 // togle class active untuk search form
 const searchform = document.querySelector('.search-form');
-const searchbox = document.querySelector('.#search-box');
+const searchbox = document.querySelector('#search-box');
 
 document.querySelector('#search-button').onclick = () => {
     searchform.classList.toggle('active');
